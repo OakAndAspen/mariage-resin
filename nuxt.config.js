75a48d0a90b21b3df10affff20512b534f8ee1d1
Irina Despot <irinadespot@gmail.com>
2021-02-15 22:24:34 +0100

    Fonts import & htaccess

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,15 @@ export default {
             {hid: 'description', name: 'description', content: ''}
         ],
         link: [
-            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=Caveat&display=swap'
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz:wght@300;400;600&display=swap'
+            }
         ]
     },
     css: [
